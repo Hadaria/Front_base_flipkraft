c2b45aa42fd11168f7858192e15ed77076ae9424
lourdvic <lourdvic.alcindor@epitech.eu>
2019-09-24 22:40:28 +0200

    récupération des users existant par call d'api

--- src/pages/Profile/UserProfile.js
+++ src/pages/Profile/UserProfile.js
@@ -1,7 +1,9 @@
-import React, { Component, PropsTypes} from "react";
+import React from "react";
 import NavBar from "../../components/NavBar";
 import {HashRouter as Router} from "react-router-dom";
 import {UserAuthWrapper} from 'redux-auth-wrapper';
+import {forEach} from "react-bootstrap/utils/ElementChildren";
+import {func} from "prop-types";
 
 //redirect to login by default
 /*const UserIsAuthenticated = UserAuthWrapper({
@@ -13,17 +15,46 @@ import {UserAuthWrapper} from 'redux-auth-wrapper';
 */
 
 class UserProfile extends  React.Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            users: []
+
+        }
+    }
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("http://test.flipkraft.ovh/api/user")
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    loading: false,
+                    users: response
+                })
+            })
+    }
     render() {
         return (
-            <div classname="Sign">
+            <div>
                 <NavBar />
-                <div className="Sign__Aside">
-                    <div className="Sign__Form">
-                        <Profil profil={{name: "Holy", imgUrl:"https://cdn4.vectorstock.com/i/thumb-large/70/33/avatar-hacker-man-vector-10607033.jpg"}}/>
-                        <Params params={{topics: "42", posts: "142", date:"15 Septembre 2012"}}/>
+                <div className="Sign">
+                    <div className="Sign__Aside">
+                        <div className="Sign__Form">
+                            <Profil profil={{name: "Holy", imgUrl:"https://cdn4.vectorstock.com/i/thumb-large/70/33/avatar-hacker-man-vector-10607033.jpg"}}/>
+                                {this.state.users.map(function (item, index) {
+                                    return (
+                                        <div key={index}>
+                                            <h1>{item.username}</h1>
+                                        </div>
+                                    )
+                                })}
+                            <Params params={{topics: "42", posts: "142", date:"15 Septembre 2012"}}/>
+                        </div>
                     </div>
                 </div>
             </div>
+
         );
     }
 }
@@ -51,3 +82,4 @@ function Params(props) {
         </div>
     )
 }
+
